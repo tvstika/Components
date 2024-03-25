19cb3821897828bc745413b32b642df194eee506
Vivi Ferliana Putri <tustika.nityasa@gmail.com>
2024-03-25 14:58:11 +0700

    add dark mode & product images

---
+++ js/mode.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body")
+const elements = document.querySelectorAll(".footer-section, .navbar");
+const defaultMod = body.style.backgroundImage
+const input = document.querySelector(".form-check-input")
+
+let dark = () => {
+  const btn = document.body
+  btn.classList.toggle("dark")
+  if (body.classList.contains("dark")) {
+    body.style.cssText = `background-image : none;`
+    body.style = `background-color : none;`
+    input.style.backgroundColor = "#292929"
+    elements.forEach(element => {
+        element.style.backgroundColor = '#565656';
+    });
+  } else {
+    body.style.backgroundImage = defaultMod
+    input.style.backgroundColor = ""
+    elements.forEach(element => {
+        element.style.backgroundColor = 'rgba(217, 217, 217, 1)';
+    });
+  }
+}
